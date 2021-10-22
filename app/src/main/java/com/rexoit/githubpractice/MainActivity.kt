b7206657d4442ab7333b86e19cfa06b18d4205f2
ebrahim <ebrahimrahman20@gmail.com>
2021-10-22 11:27:04 +0600

    ebrahim first commit

--- app/src/main/java/com/rexoit/githubpractice/MainActivity.kt
+++ app/src/main/java/com/rexoit/githubpractice/MainActivity.kt
@@ -2,10 +2,15 @@ package com.rexoit.githubpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
+private const val TAG = "mainActivity"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "onCreate: Ebrahim")
+
     }
 }
