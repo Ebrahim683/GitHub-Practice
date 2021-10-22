c870baa774cc7d0271d7f41222f5a14298c63d34
ebrahim <ebrahimrahman20@gmail.com>
2021-10-22 11:55:24 +0600

    master 7

---
+++ app/src/main/java/com/rexoit/githubpractice/MasterActivity1.kt
@@ -0,0 +1,11 @@
+package com.rexoit.githubpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MasterActivity1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_master1)
+    }
+}
